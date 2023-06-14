3cc193fbf51cee795569131f1dbb7831097b1a98
ChangHoon <hyo04044@naver.com>
2023-06-14 16:43:45 +0900

    Di Option

--- src/test/java/hello/core/singletone/SingletonTest.java
+++ src/test/java/hello/core/singletone/SingletonTest.java
@@ -49,7 +49,7 @@ public class SingletonTest {
 
     @Test
     @DisplayName("스프링 컨테이너와 싱글톤")
-    void springContainer(){
+    void springContaoiner(){
 
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService1 = ac.getBean("memberService", MemberService.class);
